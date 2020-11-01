5453f372b34c8b04532504c7548f4066c60563dd
Peter Irvine <peirvine@gmail.com>
2020-10-31 21:51:22 -0500

    Mostly done frontpage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,13 +6,15 @@ import "./Home.css"
 
 export default function Home() {
   const howWeMet= {
+    title: 'How We Met',
     date: "4.23.14",
-    story: "some story",
+    body: "We met at a classmate’s party and didn’t think much of it. Then a bunch of us went to karaoke after a long day of back-to-back classes and we both picked, “I’d Do Anything for Love.” And it just clicked. He went low, I went high. There were fist pumps. We harmonized by accident. That’s pretty much when we thought, “Okay. This could be something.”",
   }
 
   const proposal= {
+    title: 'The Proposal',
     date: "4.23.14",
-    story: "some story",
+    body: "On the anniversary of when we first met, we went back to our old stomping grounds to do karaoke with friends. When 'I'd Do Anything for Love' came on, we ran up to the front. When the music suddenly died down, I looked over and she was down on one knee. I screamed, shouted YES, and we were engaged. We're so excited to celebrate this next chapter in our lives with you. See you in San Fran!",
   }
 
   return (
--- src/components/split/Split.js
+++ src/components/split/Split.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import "./Split.css"
+
 export default function Split(payload) {
   return (
-    <>
-    <p> left {payload.date && (<p>{payload.date}</p>)} </p>
-    <p> right </p>
-    </>
+    <div className="split">
+      <div className="splitLeft">
+        <p className="splitTitle"> {payload.title && (<p>{payload.title}</p>)} </p>
+        <p className="splitDate"> {payload.date && (<p>{payload.date}</p>)} </p>
+      </div>
+      <div className="splitRight">
+        <p className="splitBody"> {payload.body && (<p>{payload.body}</p>)} </p>
+      </div>
+    </div>
   )
 }
