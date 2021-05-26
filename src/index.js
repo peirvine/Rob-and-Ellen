0b54d0a9c8327d623cbb8f4205b462ad88e5e7da
Peter Irvine <peirvine@gmail.com>
2021-05-26 08:43:41 -0500

    schedule and rsvp

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import Header from './components/header/Header'
 import Schedule from './components/schedule/Schedule'
 import Hotels from './components/hotels/Hotels'
 import Registry from './components/registry/Registry'
-import WeddingParty from './components/weddingparty/WeddingParty'
 import Photos from './components/photos/Photos'
 import Rsvp from './components/rsvp/RSVP'
 import Footer from './components/footer/Footer'
@@ -35,9 +34,8 @@ ReactDOM.render(
               <Nav.Link><Link to="/schedule">Schedule</Link></Nav.Link>
               <Nav.Link><Link to="/hotels">Hotels</Link></Nav.Link>
               <Nav.Link><Link to="/registry">Registry</Link></Nav.Link>
-              {/*<Nav.Link><Link to="/wedding-party">Wedding Party</Link></Nav.Link>*/}
               <Nav.Link><Link to="/photos">Photos</Link></Nav.Link>
-              {/*<Nav.Link><Link to="/rsvp">RSVP</Link></Nav.Link>*/}
+              <Nav.Link href="https://ellenandrob.anrsvp.com/" target="_blank" rel="noreferrer" as="a">RSVP</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
@@ -52,9 +50,6 @@ ReactDOM.render(
           </Route>
           <Route path="/registry">
             <Registry />
-          </Route>
-          <Route path="/wedding-party">
-            <WeddingParty />
           </Route>
           <Route path="/photos">
             <Photos />
